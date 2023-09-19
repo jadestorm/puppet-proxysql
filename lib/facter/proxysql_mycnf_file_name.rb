fbb6de5e885754cad3251c4f23493ff6b92cd336
Daniel Henninger <daniel@ncsu.edu>
2023-09-19 11:49:05 -0400

    Missed part of the fact.

--- lib/facter/proxysql_mycnf_file_name.rb
+++ lib/facter/proxysql_mycnf_file_name.rb
@@ -2,3 +2,8 @@ Facter.add(:proxysql_mycnf_file_name) do
   setcode do
     if File.exist? '/root/.proxysql_mycnf_file_name'
       Facter::Core::Execution.execute('cat /root/.proxysql_mycnf_file_name')
+    else
+      '/root/.my.cnf'
+    end
+  end
+end
